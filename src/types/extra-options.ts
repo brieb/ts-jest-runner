a954f9f81c9d9a0552f9319419801bb239dbff93
Brie <723547+brieb@users.noreply.github.com>
2018-12-05 15:13:53 -0800

    Expand type for local extra options

--- src/types/extra-options.ts
+++ src/types/extra-options.ts
@@ -4,5 +4,11 @@ export interface ExtraOptions {
   tsCompilerOptions: ts.CompilerOptions;
 }
 
-/** Local extra options for consumers. Must be serializable. */
-export type LocalExtraOptions = { [key: string]: string | number };
+/** Local extra options for consumers. Must be JSON-serializable. */
+export type LocalExtraOptions = JSONObject;
+
+// https://github.com/Microsoft/TypeScript/issues/1897#issuecomment-331765301
+type JSONPrimitive = string | number | boolean | null | undefined;
+type JSONValue = JSONPrimitive | JSONObject | JSONArray;
+type JSONObject = { [member: string]: JSONValue };
+interface JSONArray extends Array<JSONValue> {}
